fcdeca60b30186146b9c4ea7306ec2629588a946
josegre1 <josegre1@hotmail.com>
2017-02-09 03:25:37 -0500

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -36,17 +36,17 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 
   gem 'rspec-rails', '~> 3.5'
 end
 
+# Access an IRB console on exception pages or by using <%= console %> in views
+gem 'web-console', '~> 2.0', group: :development
+
 group :test do
-  gem 'capybara'
+  gem 'capybara' , '2.2'
 end
 
 group :production do
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Static Page" do
+  describe "Home Page" do
+    it "should have the content 'Sample App'"  do
+      visit '/static_pages/home'
+      page.should have_selector('h1',
+        :text => "Sample App")
+    end
+    it "'Home' should have the right title" do
+      visit '/static_pages/home'
+      page.should have_title(" | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1',
+        :text => "Help")
+    end
+    it "'Help' should have the right title" do
+      visit '/static_pages/help'
+      page.should have_title(" | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1',
+        :text => "About Us")
+    end
+    it "'About Us' should have the right title" do
+      visit '/static_pages/about'
+      page.should have_title(" | About Us")
+    end
+  end
+end
