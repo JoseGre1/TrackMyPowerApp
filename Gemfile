9d9a67ac6f9e19c24cf71367c7fe444d9a627559
josegre1 <josegre1@hotmail.com>
2017-02-09 20:11:54 -0500

    Starting section 3.6.3 - Hartl Rails Book

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
-# Use sqlite3 as the database for Active Record
+# Use PostgreSQL as Active Records database
+gem 'pg'
+#Use sqlite3
 gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -38,17 +39,15 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-
-  gem 'rspec-rails', '~> 3.5'
+  gem 'guard-rspec'
 end
 
 # Access an IRB console on exception pages or by using <%= console %> in views
 gem 'web-console', '~> 2.0', group: :development
 
 group :test do
+  gem 'rspec-rails', '~> 3.5'
   gem 'capybara' , '2.2'
-end
-
-group :production do
-  gem 'pg'
+  gem 'rb-inotify'
+  gem 'libnotify'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Static Page" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home Page" do
     it "should have the content 'Sample App'"  do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ RSpec.describe "Static Page" do
     end
     it "'Home' should have the right title" do
       visit '/static_pages/home'
-      page.should have_title(" | Home")
+      page.should have_title("#{base_title} | Home")
     end
   end
 
@@ -21,7 +23,7 @@ RSpec.describe "Static Page" do
     end
     it "'Help' should have the right title" do
       visit '/static_pages/help'
-      page.should have_title(" | Help")
+      page.should have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +35,7 @@ RSpec.describe "Static Page" do
     end
     it "'About Us' should have the right title" do
       visit '/static_pages/about'
-      page.should have_title(" | About Us")
+      page.should have_title("#{base_title} | About Us")
     end
   end
 end
