c45e04f9b8763f2fc11672ee7603e660d7765291
josegre1 <josegre1@hotmail.com>
2017-03-05 14:56:11 -0500

    Adding partials, contact page, new tests and user controller with new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'users/new'
+
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Static Page" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+RSpec.describe "Static Pages" do
+  #let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home Page" do
-    it "should have the content 'Sample App'"  do
-      visit '/static_pages/home'
-      page.should have_selector('h1',
-        :text => "Sample App")
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_title("Ruby on Rails Tutorial Sample App")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_title("| Home")
-    end
+    before { visit root_path }
+
+    it { should have_selector('h1', :text => "Sample App") }
+    it { should have_title(full_title('')) }
+    it { should_not have_title("| Home") }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1',
-        :text => "Help")
-    end
-    it "'Help' should have the right title" do
-      visit '/static_pages/help'
-      page.should have_title("#{base_title} | Help")
-    end
+    before { visit help_path }
+    it { should have_selector('h1', :text => "Help") }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1',
-        :text => "About Us")
-    end
-    it "'About Us' should have the right title" do
-      visit '/static_pages/about'
-      page.should have_title("#{base_title} | About Us")
-    end
+    before { visit about_path }
+    it { should have_selector('h1', :text => "About") }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1', :text => "Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
