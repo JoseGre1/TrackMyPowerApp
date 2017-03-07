e28b5eab392f077327d0dabe54d1f96b955c4392
josegre1 <josegre1@hotmail.com>
2017-03-06 23:17:42 -0500

    database updated for local - user RSpec created

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "User pages" do
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
