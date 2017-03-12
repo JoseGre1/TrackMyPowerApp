54c61fe644a833c49950fc3adab81f04c74978f1
josegre1 <josegre1@hotmail.com>
2017-03-12 06:20:11 -0500

    Starting signup form 7.2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # The priority is based upon order of creation: first created -> highest priority.
