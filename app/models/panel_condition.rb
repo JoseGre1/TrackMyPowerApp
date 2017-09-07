7fdfa41a3c6f483d19ba708d277175e322b3782d
AlexaLopezR <alexacaro14@gmail.com>
2017-09-07 17:53:02 -0500

    Default values correction

--- app/models/panel_condition.rb
+++ app/models/panel_condition.rb
@@ -2,8 +2,8 @@ class PanelCondition < ApplicationRecord
   before_save :panel_condition
   protected
     def panel_condition
-          self.temp_ext = 0
-          self.temp_panel = 0
-          self.radiation = 0
+          self.temp_ext ||= 0
+          self.temp_panel ||= 0
+          self.radiation ||= 0
      end
 end
