d306748fc7d14d90fa2b8964c313cf6af59b3385
equipohar <equipohar@gmail.com>
2017-09-10 01:48:30 -0500

    Generating a model named TrainingDataMeasurement with its respective migrations. In order to do that the previous model was deleted

--- app/models/training_data_measurement.rb
+++ app/models/training_data_measurement.rb
@@ -1,2 +1,10 @@
 class TrainingDataMeasurement < ApplicationRecord
+	 before_save :default_values
+  protected
+    def default_values
+      self.x ||= 0
+      self.y ||= 0
+      self.z||= 0
+      self.label ||= 0
+    end
 end
