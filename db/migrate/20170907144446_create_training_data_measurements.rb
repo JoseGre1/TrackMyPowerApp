f707188cd0b9ed6d987acde801823c009268ed45
equipohar <equipohar@gmail.com>
2017-09-10 22:23:36 -0500

    Deleting these migrations because they are wrong. The correct migrations where added in a previous commit

--- db/migrate/20170907144446_create_training_data_measurements.rb
+++
@@ -1,8 +0,0 @@
-class CreateTrainingDataMeasurements < ActiveRecord::Migration[5.0]
-  def change
-    create_table :training_data_measurements do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170907144901_add_columns_to_training_data_measurement.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToTrainingDataMeasurement < ActiveRecord::Migration[5.0]
-  def change
-    add_column :training_data_measurements, :x, :double
-    add_column :training_data_measurements, :y, :double
-    add_column :training_data_measurements, :z, :double
-    add_column :training_data_measurements, :label, :string
-  end
-end
