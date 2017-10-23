e65e75ffc1b7b84e825984201828a58fe19affb6
vipenergyun <mpardo@uninorte.edu.co>
2017-10-23 15:52:00 -0500

    New table Wind turbine Frequencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919155353) do
+ActiveRecord::Schema.define(version: 20171023184755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -213,15 +213,9 @@ ActiveRecord::Schema.define(version: 20170919155353) do
   create_table "wind_turbine_vibration_measurements", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float    "max_ejex"
     t.float    "m_ejex"
-    t.float    "min_ejex"
-    t.float    "max_ejey"
     t.float    "m_ejey"
-    t.float    "min_ejey"
-    t.float    "max_ejez"
     t.float    "m_ejez"
-    t.float    "min_ejez"
   end
 
   add_foreign_key "alerts", "users"
