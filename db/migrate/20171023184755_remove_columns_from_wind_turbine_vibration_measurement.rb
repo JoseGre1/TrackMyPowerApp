6dae17404c5ec5ee8ab13515992e5808e74352c3
AllisonFdez <fdezlobo.al@gmail.com>
2017-10-23 13:49:51 -0500

    Migration, remove columns from Wind Turbine Vibration table

---
+++ db/migrate/20171023184755_remove_columns_from_wind_turbine_vibration_measurement.rb
@@ -0,0 +1,10 @@
+class RemoveColumnsFromWindTurbineVibrationMeasurement < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :wind_turbine_vibration_measurements, :max_ejex, :float,
+    remove_column :wind_turbine_vibration_measurements, :min_ejex, :float,
+    remove_column :wind_turbine_vibration_measurements, :max_ejey, :float,
+    remove_column :wind_turbine_vibration_measurements, :min_ejey, :float,
+    remove_column :wind_turbine_vibration_measurements, :max_ejez, :float,
+    remove_column :wind_turbine_vibration_measurements, :min_ejez, :float
+  end
+end
